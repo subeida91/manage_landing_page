8d4a9a8b2afc192d7ab709c9032eb65f15d83415
subeida91 <subeida91@gmail.com>
2020-10-03 15:49:50 +0100

    added an overlay

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,21 @@
-console.log("helo")
+const btnhamburger = document.querySelector("#btnHamburger");
+const header = document.querySelector(".header");
+const overlay = document.querySelector('.overlay');
+
+btnhamburger.addEventListener('click', function(){
+    console.log("click hamburger");
+
+    if(header.classList.contains('open')){ //close hamburger menu
+        header.classList.remove('open');
+        overlay.classList.add("fade-out");
+        overlay.classList.remove("fade-in");
+
+        
+    }
+    else{ //open hamburger menu
+        header.classList.add('open');
+        overlay.classList.add("fade-in");
+        overlay.classList.remove("fade-out")
+    }
+    
+});
